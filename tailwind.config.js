73d79872709bcf43add830364761407bd10471fa
MUSARA TECHNOLOGIES <musamunyah67@gmail.com>
2026-08-17 21:09:07 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#1e3a72',
+        primary: '#1547e7',
         gold: '#ffaf59',
         'dark-gray': '#a89bba',
       },
