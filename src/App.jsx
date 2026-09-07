578a5a585955f2b45252b2ad3a53264f3c172b45
MUSARA TECHNOLOGIES <musamunyah67@gmail.com>
2026-09-07 07:58:17 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import { SiteSettingsProvider } from './contexts/SiteSettingsContext'
 import { Toaster } from 'react-hot-toast'
 import NavigationProgressBar from './components/NavigationProgressBar'
 import MaintenanceGuard from './components/MaintenanceGuard'
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <NavigationProgressBar />
-        <MaintenanceGuard />
-        <Toaster position="top-right" />
-      </Router>
-    </AuthProvider>
+    <SiteSettingsProvider>
+      <AuthProvider>
+        <Router>
+          <NavigationProgressBar />
+          <MaintenanceGuard />
+          <Toaster position="top-right" />
+        </Router>
+      </AuthProvider>
+    </SiteSettingsProvider>
   )
 }
 
